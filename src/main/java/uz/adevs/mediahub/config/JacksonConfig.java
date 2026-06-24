e04cdc960b3fd3913423f273d4b8708c72909d76
Akhror <axror.gulomovv@gmail.com>
2026-06-24 14:49:54 +0500

    api issues

---
+++ src/main/java/uz/adevs/mediahub/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+package uz.adevs.mediahub.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Hibernate6Module hibernate6Module() {
+        Hibernate6Module module = new Hibernate6Module();
+        // Serialize lazy-loaded proxies as null instead of throwing an error
+        module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
+        return module;
+    }
+}
